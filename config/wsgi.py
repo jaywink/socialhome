7ce791a76cbadec340105b686b0aa79f91a9e3a6
Jason Robinson <mail@jasonrobinson.me>
2016-04-09 17:12:05 +0300

    Fix env.local loading in wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -20,18 +20,18 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
+# Load local environment
+with open("env.local") as envfile:
+    for line in envfile:
+        if line.strip():
+            setting = line.strip().split("=", maxsplit=1)
+            os.environ.setdefault(setting[0], setting[1])
+
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
-# Load local environment
-with open("../env.local") as envfile:
-    for line in envfile:
-        if line.strip():
-            setting = line.strip().split("=", maxsplit=1)
-            os.environ.setdefault(setting[0], setting[1])
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
