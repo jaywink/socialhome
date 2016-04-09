c08209554352783c80be69a1d240dca6fd8c0a89
Jason Robinson <mail@jasonrobinson.me>
2016-04-09 13:48:31 +0300

    Make manage.py load env.local variables with option --load-env
    Good for injecting environment into PyCharm run configurations for example and no need to
    manually load the env values for manage.py commands.

--- manage.py
+++ manage.py
@@ -2,8 +2,20 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+    # Load environment variables from env.local, if option --load-env specified.
+    # Good for injecting environment into PyCharm run configurations for example and no need to
+    # manually load the env values for manage.py commands
+    if "--load-env" in sys.argv:
+        with open("env.local") as envfile:
+            for line in envfile:
+                if line.strip():
+                    setting = line.strip().split("=", maxsplit=1)
+                    os.environ.setdefault(setting[0], setting[1])
+        sys.argv.remove("--load-env")
 
     from django.core.management import execute_from_command_line
 
