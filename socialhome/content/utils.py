7728402e41c0e263d9fcb853aee2c3fe110540d7
Jason Robinson <mail@jasonrobinson.me>
2016-08-05 01:34:54 +0300

    Ensure remote content is cleaned from federation layer

--- socialhome/content/utils.py
+++ socialhome/content/utils.py
@@ -18,3 +18,7 @@ def safe_text_for_markdown_code(text):
     for counter, code in enumerate(safety, 1):
         text = text.replace("%%safe_text_for_markdown_code codes in safety %s%%" % counter, code, 1)
     return text
+
+
+def safe_text(text):
+    return bleach.clean(text, tags=[], attributes=[], styles=[], strip=True)
