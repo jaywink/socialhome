1b9f5e8dfa7927057fc5bb63ea8195048f36d3ce
Jason Robinson <mail@jasonrobinson.me>
2016-09-12 01:47:09 +0300

    Make manage.py always load environment values
    And also warn if env.local hasn't been created yet.

--- manage.py
+++ manage.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
 
 
 if __name__ == "__main__":
-    # Load environment variables from env.local, if option --load-env specified.
+    # Load environment variables from env.local.
     # Good for injecting environment into PyCharm run configurations for example and no need to
     # manually load the env values for manage.py commands
-    if "--load-env" in sys.argv:
+    try:
         with open("env.local") as envfile:
             for line in envfile:
                 if line.strip():
                     setting = line.strip().split("=", maxsplit=1)
                     os.environ.setdefault(setting[0], setting[1])
-        sys.argv.remove("--load-env")
+        # Backwards compatibility, since we used to require this and Django will freak out if here
+        # TODO: Remove in some future date
+        if "--load-env" in sys.argv:
+            warnings.warn("Argument '--load-env' has been deprecated and will be removed in the future.")
+            sys.argv.remove("--load-env")
+    except FileNotFoundError:
+        warnings.warn("Config file 'env.local' is missing. It should be created from 'env.example' with "
+                      "necessary values replaced.")
 
     # Fail safe settings module
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
