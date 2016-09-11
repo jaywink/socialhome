d716cc27e39697bac3ecd529730d2bb601802537
Jason Robinson <mail@jasonrobinson.me>
2016-09-11 23:43:52 +0300

    Add more tests for content sending related changes

--- socialhome/content/tests/test_signals.py
+++ socialhome/content/tests/test_signals.py
@@ -5,10 +5,11 @@ import pytest
 from test_plus import TestCase
 
 from socialhome.content.tests.factories import ContentFactory
+from socialhome.users.tests.factories import UserFactory
 
 
 @pytest.mark.usefixtures("db")
-class TestContentModel(TestCase):
+class TestNotifyListeners(TestCase):
     @patch("socialhome.content.signals.StreamConsumer")
     def test_content_save_calls_streamconsumer_group_send(self, mock_consumer):
         mock_consumer.group_send = Mock()
@@ -21,3 +22,17 @@ class TestContentModel(TestCase):
         content.text = "foo"
         content.save()
         self.assertFalse(mock_consumer.group_send.called)
+
+
+@pytest.mark.usefixtures("db")
+@patch("socialhome.content.signals.send_content.delay")
+class TestFederateContent(TestCase):
+    def test_non_local_content_does_not_get_sent(self, mock_send):
+        ContentFactory()
+        mock_send.assert_not_called()
+
+    def test_local_content_gets_sent(self, mock_send):
+        user = UserFactory()
+        content = ContentFactory(author=user.profile)
+        self.assertTrue(content.is_local)
+        mock_send.assert_called_once_with(content)
