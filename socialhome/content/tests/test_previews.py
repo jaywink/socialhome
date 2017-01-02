fdb9e175a01e1cd4a669b70fb969d5384211eda4
Jason Robinson <mail@jasonrobinson.me>
2017-01-02 23:29:20 +0200

    Add tests for fetch_content_preview
    Refs: #16

--- socialhome/content/tests/test_previews.py
+++ socialhome/content/tests/test_previews.py
@@ -1,14 +1,46 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
+from socialhome.content.previews import fetch_content_preview
+from socialhome.content.tests.factories import ContentFactory
 
-class FetchOgPreviewTestCase(TestCase):
+
+class TestFetchOgPreview(TestCase):
     @classmethod
     def setUpTestData(cls):
-        super(FetchOgPreviewTestCase, cls).setUpTestData()
+        super(TestFetchOgPreview, cls).setUpTestData()
 
     def setUp(self):
-        super(FetchOgPreviewTestCase, self).setUp()
+        super(TestFetchOgPreview, self).setUp()
 
-    def test_og_does_not_remember_old_values(self):
-        # TODO
-        pass
+
+class TestFetchContentPreview(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super(TestFetchContentPreview, cls).setUpTestData()
+        cls.content = ContentFactory()
+
+    @patch("socialhome.content.previews.find_urls_in_text", return_value=[])
+    @patch("socialhome.content.previews.fetch_oembed_preview")
+    @patch("socialhome.content.previews.fetch_og_preview")
+    def test_find_urls_in_text_called(self, fetch_og, fetch_oembed, find_urls):
+        fetch_content_preview(self.content)
+        find_urls.assert_called_once_with(self.content.text)
+        fetch_oembed.assert_not_called()
+        fetch_og.assert_not_called()
+
+    @patch("socialhome.content.previews.find_urls_in_text", return_value=["example.com"])
+    @patch("socialhome.content.previews.fetch_oembed_preview", return_value="fooo")
+    @patch("socialhome.content.previews.fetch_og_preview")
+    def test_fetch_oembed_preview_called(self, fetch_og, fetch_oembed, find_urls):
+        fetch_content_preview(self.content)
+        fetch_oembed.assert_called_once_with(self.content, ["example.com"])
+        fetch_og.assert_not_called()
+
+    @patch("socialhome.content.previews.find_urls_in_text", return_value=["example.com"])
+    @patch("socialhome.content.previews.fetch_oembed_preview", return_value=None)
+    @patch("socialhome.content.previews.fetch_og_preview")
+    def test_fetch_og_preview_called(self, fetch_og, fetch_oembed, find_urls):
+        fetch_content_preview(self.content)
+        fetch_og.assert_called_once_with(self.content, ["example.com"])
