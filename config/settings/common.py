9c363d2b1a9c576f3f9bf1a6a5ee7695c83d10ac
Jason Robinson <mail@jasonrobinson.me>
2017-01-07 02:51:33 +0200

    Make env.local to be automatically loaded
    Closes #80

--- config/settings/common.py
+++ config/settings/common.py
@@ -10,12 +10,17 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 from __future__ import absolute_import, unicode_literals
 
+import os
+
 import environ
 
 ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
 APPS_DIR = ROOT_DIR.path('socialhome')
 
+# Local environment
+# -----------------
 env = environ.Env()
+env.read_env('env.local')
 
 # APP CONFIGURATION
 # ------------------------------------------------------------------------------
--- manage.py
+++ manage.py
@@ -1,29 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import warnings
 
 
 if __name__ == "__main__":
-    # Load environment variables from env.local.
-    # Good for injecting environment into PyCharm run configurations for example and no need to
-    # manually load the env values for manage.py commands
-    try:
-        with open("env.local") as envfile:
-            for line in envfile:
-                if line.strip():
-                    setting = line.strip().split("=", maxsplit=1)
-                    os.environ.setdefault(setting[0], setting[1])
-        # Backwards compatibility, since we used to require this and Django will freak out if here
-        # TODO: Remove in some future date
-        if "--load-env" in sys.argv:
-            warnings.warn("Argument '--load-env' has been deprecated and will be removed in the future.")
-            sys.argv.remove("--load-env")
-    except FileNotFoundError:
-        warnings.warn("Config file 'env.local' is missing. It should be created from 'env.example' with "
-                      "necessary values replaced.")
-
-    # Fail safe settings module
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     from django.core.management import execute_from_command_line
