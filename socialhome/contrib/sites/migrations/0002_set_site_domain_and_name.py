1f1b34ee51d4ccb907651d9076e1e0b4aaa77a74
Jason Robinson <mail@jasonrobinson.me>
2017-01-29 22:06:54 +0200

    Fix oops domain in old contrib.sites migration

--- socialhome/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ socialhome/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -17,7 +17,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "jasonrobinson.me",
+            "domain": "socialhome.network",
             "name": "Socialhome"
         }
     )
