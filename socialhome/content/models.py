86848511f249ecb3271f5f9f72feb63feb1e0eba
Jason Robinson <mail@jasonrobinson.me>
2017-04-06 23:06:02 +0300

    Refactor Content querysets to own file and add tests

--- socialhome/content/models.py
+++ socialhome/content/models.py
@@ -6,7 +6,7 @@ import arrow
 from CommonMark import commonmark
 from django.conf import settings
 from django.db import models
-from django.db.models import Q
+from django.db.models.aggregates import Max
 from django.template.loader import render_to_string
 from django.urls import NoReverseMatch
 from django.urls import reverse
@@ -16,8 +16,8 @@ from django.utils.translation import ugettext_lazy as _
 from django_extensions.utils.text import truncate_letters
 from enumfields import EnumIntegerField
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
-from django.db.models.aggregates import Max
 
+from socialhome.content.querysets import TagQuerySet, ContentQuerySet
 from socialhome.content.utils import make_nsfw_safe, test_tag
 from socialhome.enums import Visibility
 from socialhome.users.models import Profile
@@ -46,18 +46,6 @@ class OEmbedCache(models.Model):
         return self.url
 
 
-class TagQuerySet(models.QuerySet):
-    def get_by_cleaned_name(self, name):
-        """Get by name after making sure it's lower case and trimmed."""
-        cleaned = name.strip().lower()
-        return self.get(name=cleaned)
-
-    def exists_by_cleaned_name(self, name):
-        """Exists filter by name after making sure it's lower case and trimmed."""
-        cleaned = name.strip().lower()
-        return self.filter(name=cleaned).exists()
-
-
 class Tag(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
     created = AutoCreatedField(_('Created'))
@@ -75,42 +63,6 @@ class Tag(models.Model):
         """
         self.name = self.name.strip().lower()
         super().save()
-
-
-class ContentQuerySet(models.QuerySet):
-    def _select_related(self):
-        return self.select_related("oembed", "opengraph")
-
-    def visible_for_user(self, user):
-        """Filter by visibility to given user."""
-        if not user.is_authenticated:
-            return self.filter(visibility=Visibility.PUBLIC)
-        # TODO: handle also LIMITED when contacts implemented
-        return self.filter(Q(author=user.profile) | Q(visibility__in=[Visibility.SITE, Visibility.PUBLIC]))
-
-    def public(self):
-        return self._select_related().filter(visibility=Visibility.PUBLIC).order_by("-created")
-
-    def tags(self, tag, user):
-        try:
-            tag = Tag.objects.get_by_cleaned_name(tag)
-        except Tag.DoesNotExist:
-            return Content.objects.none()
-        return self._select_related().visible_for_user(user).filter(tags=tag).order_by("-created")
-
-    def profile(self, guid, user):
-        """Filter for a user profile.
-
-        Ensures if the profile is not visible to the user, no content will be returned.
-        """
-        try:
-            profile = Profile.objects.get(guid=guid)
-        except Profile.DoesNotExist:
-            return Content.objects.none()
-        if not profile.visible_to_user(user):
-            return Content.objects.none()
-        qs = self._select_related().visible_for_user(user).filter(pinned=True, author=profile)
-        return qs.order_by("order")
 
 
 class Content(models.Model):
---
+++ socialhome/content/querysets.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.db.models import Q
+
+from socialhome.enums import Visibility
+from socialhome.users.models import Profile
+
+
+class TagQuerySet(models.QuerySet):
+    def get_by_cleaned_name(self, name):
+        """Get by name after making sure it's lower case and trimmed."""
+        cleaned = name.strip().lower()
+        return self.get(name=cleaned)
+
+    def exists_by_cleaned_name(self, name):
+        """Exists filter by name after making sure it's lower case and trimmed."""
+        cleaned = name.strip().lower()
+        return self.filter(name=cleaned).exists()
+
+
+class ContentQuerySet(models.QuerySet):
+    def _select_related(self):
+        return self.select_related("oembed", "opengraph")
+
+    def visible_for_user(self, user):
+        """Filter by visibility to given user."""
+        if not user.is_authenticated:
+            return self.filter(visibility=Visibility.PUBLIC)
+        # TODO: handle also LIMITED when contacts implemented
+        return self.filter(Q(author=user.profile) | Q(visibility__in=[Visibility.SITE, Visibility.PUBLIC]))
+
+    def public(self):
+        return self._select_related().filter(visibility=Visibility.PUBLIC).order_by("-created")
+
+    def tags(self, tag, user):
+        from socialhome.content.models import Tag, Content
+        try:
+            tag = Tag.objects.get_by_cleaned_name(tag)
+        except Tag.DoesNotExist:
+            return Content.objects.none()
+        return self._select_related().visible_for_user(user).filter(tags=tag).order_by("-created")
+
+    def profile(self, guid, user):
+        """Filter for a user profile.
+
+        Ensures if the profile is not visible to the user, no content will be returned.
+        """
+        from socialhome.content.models import Content
+        try:
+            profile = Profile.objects.get(guid=guid)
+        except Profile.DoesNotExist:
+            return Content.objects.none()
+        if not profile.visible_to_user(user):
+            return Content.objects.none()
+        qs = self._select_related().visible_for_user(user).filter(pinned=True, author=profile)
+        return qs.order_by("order")
