fe08c1e65554f7991cf8df6e47cbd5a44f214c4e
Jason Robinson <mail@jasonrobinson.me>
2017-07-07 11:49:31 +0300

    Add tests for HomeView rendering based on landing page pref

--- socialhome/tests/test_views.py
+++ socialhome/tests/test_views.py
@@ -1,24 +1,23 @@
-import pytest
 from django.test import override_settings
 
+from socialhome.content.tests.factories import ContentFactory
 from socialhome.enums import Visibility
+from socialhome.streams.views import FollowedStreamView, PublicStreamView
 from socialhome.tests.utils import SocialhomeTestCase
 from socialhome.users.models import Profile
 from socialhome.users.tests.factories import UserFactory, AdminUserFactory
+from socialhome.users.views import ProfileDetailView, ProfileAllContentView
 
 
-@pytest.mark.usefixtures("db")
-class TestBaseView(object):
-    @pytest.mark.usefixtures("client", "settings")
-    def test_signup_link_do_not_show_when_signup_are_closed(self, client, settings):
-        settings.ACCOUNT_ALLOW_REGISTRATION = False
-        response = client.get("/")
+class TestBaseView(SocialhomeTestCase):
+    @override_settings(ACCOUNT_ALLOW_REGISTRATION=False)
+    def test_signup_link_do_not_show_when_signup_are_closed(self):
+        response = self.client.get("/")
         assert "/accounts/signup/" not in str(response.content)
 
-    @pytest.mark.usefixtures("client", "settings")
-    def test_signup_link_shows_when_signup_are_opened(self, client, settings):
-        settings.ACCOUNT_ALLOW_REGISTRATION = True
-        response = client.get("/")
+    @override_settings(ACCOUNT_ALLOW_REGISTRATION=True)
+    def test_signup_link_shows_when_signup_are_opened(self):
+        response = self.client.get("/")
         assert "/accounts/signup/" in str(response.content)
 
 
@@ -49,3 +48,41 @@ class TestRootProfile(SocialhomeTestCase):
             response = self.client.get("/")
         assert response.templates[0].name == "streams/profile.html"
         assert response.context["profile"].user.username == "admin"
+
+
+class TestHomeViewLandingPagePreference(SocialhomeTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.user = UserFactory()
+        ContentFactory(pinned=True, author=cls.user.profile)  # To ensure profile view can render
+
+    def test_renders_profile(self):
+        self.user.preferences["generic__landing_page"] = "profile"
+        with self.login(self.user):
+            self.get("home")
+        self.assertEqual(self.context["view"].__class__, ProfileDetailView)
+
+    def test_renders_profile_all(self):
+        self.user.preferences["generic__landing_page"] = "profile_all"
+        with self.login(self.user):
+            self.get("home")
+        self.assertEqual(self.context["view"].__class__, ProfileAllContentView)
+
+    def test_renders_followed_stream(self):
+        self.user.preferences["generic__landing_page"] = "followed"
+        with self.login(self.user):
+            self.get("home")
+        self.assertEqual(self.context["view"].__class__, FollowedStreamView)
+
+    def test_renders_public_stream(self):
+        self.user.preferences["generic__landing_page"] = "public"
+        with self.login(self.user):
+            self.get("home")
+        self.assertEqual(self.context["view"].__class__, PublicStreamView)
+
+    def test_renders_profile_as_fallback(self):
+        self.user.preferences["generic__landing_page"] = "foobar"
+        with self.login(self.user):
+            self.get("home")
+        self.assertEqual(self.context["view"].__class__, ProfileDetailView)
