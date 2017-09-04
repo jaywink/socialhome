44d38c87125d3eb989811d4583877d8d05088477
Jason Robinson <mail@jasonrobinson.me>
2017-09-04 23:44:02 +0300

    Remove a few unused imports

--- socialhome/streams/views.py
+++ socialhome/streams/views.py
@@ -1,8 +1,6 @@
 from braces.views import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext
 from django.views.generic import ListView
-from django.urls import reverse
 
 from socialhome.content.models import Content, Tag
 
