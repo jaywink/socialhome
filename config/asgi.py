e0dd94607a445f4a58adda1a06bb5a70f6f8a21a
Jason Robinson <mail@jasonrobinson.me>
2017-10-17 22:21:37 +0300

    Remove legacy double-loading of environment variables with wsgi and asgi
    This was in the time we didn't load the env file in the Django config, forgot to remove it.

--- config/asgi.py
+++ config/asgi.py
@@ -2,8 +2,5 @@ import os
 
 from channels.asgi import get_channel_layer
 
-from config.utils import load_local_environment
-
-load_local_environment()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 channel_layer = get_channel_layer()
