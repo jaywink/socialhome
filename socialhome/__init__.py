9019c71312428898f924d1bdbbdebfdf44c8a189
Jason Robinson <mail@jasonrobinson.me>
2018-08-29 22:30:09 +0300

    Return to development version

--- socialhome/__init__.py
+++ socialhome/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.9.3"
+__version__ = "0.10.0-dev"
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace("-", ".", 1).split(".")])
 
 default_app_config = "socialhome.apps.SocialhomeConfig"
