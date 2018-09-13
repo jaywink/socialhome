47d3038b691800bfa135a903d18d4b607778e8e9
Matthew Ellis <matt.ellis012@gmail.com>
2018-09-13 16:48:53 -0400

    Moved, rewrote test with testplus syntax

--- socialhome/tests/test_api_docs.py
+++ socialhome/tests/test_api_docs.py
@@ -3,5 +3,5 @@ from socialhome.tests.utils import SocialhomeTestCase
 
 class TestSchemaView(SocialhomeTestCase):
     def test_page_renders(self):
-        response = self.client.get("/api/")
-        self.assertEqual(response.status_code, 200)
+        self.get("api-docs")
+        self.response_200()
