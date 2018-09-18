53bcec11eec80cd6f680d2d56dda745765f8d38b
Jason Robinson <mail@jasonrobinson.me>
2018-09-18 23:26:36 +0300

    Docker tweaks and testing

--- docker/app/gunicorn.sh
+++ docker/app/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/python /app/manage.py migrate --noinput
 /usr/local/bin/python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app --capture-output
+/usr/local/bin/gunicorn config.wsgi -w 2 -b 0.0.0.0:5000 --chdir=/app --capture-output
