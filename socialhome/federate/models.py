374b52c61be9be13d7ee40a15d8c8173616dd180
Jason Robinson <mail@jasonrobinson.me>
2018-09-25 23:54:31 +0300

    Introduce activity models

---
+++ socialhome/federate/models.py
@@ -0,0 +1,17 @@
+from uuid import uuid4
+
+from django.db import models
+from enumfields import EnumField
+from model_utils.fields import AutoCreatedField, AutoLastModifiedField
+
+
+
+class Activity(models.Model):
+    actor = models.ForeignKey("users.Profile", on_delete=models.CASCADE)
+    created = AutoCreatedField()
+    modified = AutoLastModifiedField()
+    type = EnumField(ActivityType)
+    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
+
+    def __str__(self):
+        return f"{self.type} by {self.actor} ({self.uuid})"
