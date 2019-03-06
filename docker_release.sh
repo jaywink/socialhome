b7d40ce9feb950d7d7e6b208a34602fb63052073
Jason Robinson <mail@jasonrobinson.me>
2019-03-06 23:22:41 +0200

    Add possibility to set tag in docker release script

--- docker_release.sh
+++ docker_release.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+#####################
+# Release to Docker #
+#####################
+#
+# Builds the image and pushes it to Docker Hub.
+#
+# Args: tag name, defaults to "latest"
+#
+
 set -e
+
+if [[ -z "$1" ]]; then
+    tag=latest
+else
+    tag=$1
+fi
 
 docker login
 
-docker build -f docker/app/Dockerfile -t jaywink/socialhome .
-docker push jaywink/socialhome:latest
+docker build -f docker/app/Dockerfile -t jaywink/socialhome:${tag} .
+docker push jaywink/socialhome:${tag}
