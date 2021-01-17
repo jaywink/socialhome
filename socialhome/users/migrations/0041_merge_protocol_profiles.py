40cfdd2abe50dee681ff862cf2946566acba3809
Jason Robinson <mail@jasonrobinson.me>
2021-01-17 04:07:56 +0200

    Fix 0041

--- socialhome/users/migrations/0041_merge_protocol_profiles.py
+++ socialhome/users/migrations/0041_merge_protocol_profiles.py
@@ -7,6 +7,7 @@ from django.db.migrations import RunPython
 def forward(apps, schema_editor):
     # noinspection PyPep8Naming
     Profile = apps.get_model("users", "Profile")
+
     Content = apps.get_model("content", "Content")
     for profile in Profile.objects.filter(user__isnull=True, protocol="activitypub").iterator():
         dupes = Profile.objects.filter(
@@ -24,10 +25,10 @@ def forward(apps, schema_editor):
             content.author_id = profile.id
             content.save()
         # - Re-assign followers and following
-        followers = Profile.objects.filter(followers_id__in=profile.id)
+        followers = Profile.objects.filter(followers__in=profile.id)
         for follower in followers:
             profile.followers.add(follower)
-        following = Profile.objects.filter(following_id__in=profile.id)
+        following = Profile.objects.filter(following__in=profile.id)
         for following in following:
             profile.following.add(following)
         # Store info
