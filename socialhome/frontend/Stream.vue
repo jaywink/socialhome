f71263007f92f6cc971e62cf2bcc0f11c8926fb7
Alain St-Denis <alain@zenfolie.org>
2021-05-18 13:12:44 +0000

    Fixed babel.cfg, was missing a few templates

---
+++ socialhome/frontend/Stream.vue
@@ -0,0 +1,155 @@
+import Vue from "vue"
+
+import StreamElement from "@/components/streams/StreamElement.vue"
+import PublicStampedElement from "@/components/streams/stamped_elements/PublicStampedElement.vue"
+import FollowedStampedElement from "@/components/streams/stamped_elements/FollowedStampedElement.vue"
+import LimitedStampedElement from "@/components/streams/stamped_elements/LimitedStampedElement.vue"
+import LocalStampedElement from "@/components/streams/stamped_elements/LocalStampedElement.vue"
+import TagStampedElement from "@/components/streams/stamped_elements/TagStampedElement.vue"
+import TagsStampedElement from "@/components/streams/stamped_elements/TagsStampedElement.vue"
+import ProfileStampedElement from "@/components/streams/stamped_elements/ProfileStampedElement.vue"
+import LoadingElement from "@/components/common/LoadingElement.vue"
+import ProfileStreamButtons from "@/components/streams/stamped_elements/ProfileStreamButtons"
+
+export default Vue.component("Stream", {
+    components: {
+        FollowedStampedElement,
+        LimitedStampedElement,
+        LoadingElement,
+        LocalStampedElement,
+        ProfileStampedElement,
+        ProfileStreamButtons,
+        PublicStampedElement,
+        StreamElement,
+        TagStampedElement,
+        TagsStampedElement,
+    },
+    // TODO: Seperate Stream.vue into TagStream.vue, GuidProfile.vue and UsernameProfile.vue, etc. in the future
+    props: {
+        contentId: {
+            type: String, default: "",
+        },
+        uuid: {
+            type: String, default: "",
+        },
+        user: {
+            type: String, default: "",
+        },
+        tag: {
+            type: String, default: "",
+        },
+    },
+    data() {
+        return {
+            masonryOptions: {
+                "item-selector": ".grid-item",
+                "column-width": ".grid-sizer",
+                gutter: ".gutter-sizer",
+                "percent-position": true,
+                "transition-duration": "0s",
+                stagger: 0,
+            },
+        }
+    },
+    computed: {
+        singleContent() {
+            if (!this.$store.state.stream.singleContentId) {
+                return null
+            }
+            return this.$store.state.stream.contents[this.$store.state.stream.singleContentId]
+        },
+        showProfileStreamButtons() {
+            return this.streamName === "profile_all" || this.streamName === "profile_pinned"
+        },
+        stampedElement() {
+            switch (this.streamName) {
+                case "followed":
+                    return "FollowedStampedElement"
+                case "limited":
+                    return "LimitedStampedElement"
+                case "local":
+                    return "LocalStampedElement"
+                case "public":
+                    return "PublicStampedElement"
+                case "tag":
+                    return "TagStampedElement"
+                case "tags":
+                    return "TagsStampedElement"
+                case "profile_all":
+                case "profile_pinned":
+                    return "ProfileStampedElement"
+                default:
+                    // eslint-disable-next-line no-console
+                    console.error(`Unsupported stream name ${this.streamName}`)
+                    return ""
+            }
+        },
+        streamName() {
+            return this.$store.state.stream.stream.name
+        },
+        translations() {
+            const ln = this.unfetchedContentIds.length
+            s = ngettext(`${ln} new post available`, `${ln} new posts available`, ln)
+            return {newPostsAvailables: s}
+        },
+        unfetchedContentIds() {
+            return this.$store.state.stream.unfetchedContentIds
+        },
+    },
+    beforeMount() {
+        if (!this.$store.state.stream.stream.single) {
+            this.loadStream()
+        }
+    },
+    methods: {
+        onNewContentClick() {
+            this.$store.dispatch("stream/newContentAck").then(
+                () => this.$nextTick( // Wait for new content to be rendered
+                    () => this.$scrollTo("body"),
+                ),
+            )
+        },
+        loadStream() {
+            const options = {params: {}}
+            const lastContentId = this.$store.state.stream.currentContentIds[
+                this.$store.state.stream.currentContentIds.length - 1
+            ]
+            if (lastContentId && this.$store.state.stream.contents[lastContentId]) {
+                options.params.lastId = this.$store.state.stream.contents[lastContentId].through
+            }
+
+            switch (this.$store.state.stream.stream.name) {
+                case "followed":
+                    this.$store.dispatch("stream/getFollowedStream", options)
+                    break
+                case "limited":
+                    this.$store.dispatch("stream/getLimitedStream", options)
+                    break
+                case "local":
+                    this.$store.dispatch("stream/getLocalStream", options)
+                    break
+                case "public":
+                    this.$store.dispatch("stream/getPublicStream", options)
+                    break
+                case "tag":
+                    options.params.name = this.tag
+                    this.$store.dispatch("stream/getTagStream", options)
+                    break
+                case "tags":
+                    this.$store.dispatch("stream/getTagsStream", options)
+                    break
+                case "profile_all":
+                    options.params.uuid = this.$store.state.application.profile.uuid
+                    this.$store.dispatch("stream/getProfileAll", options)
+                    break
+                case "profile_pinned":
+                    options.params.uuid = this.$store.state.application.profile.uuid
+                    this.$store.dispatch("stream/getProfilePinned", options)
+                    break
+                default:
+                    break
+            }
+        },
+    },
+})
+
