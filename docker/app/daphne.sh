21831e2005adde501353bd50f78c6edee29c73e9
Jason Robinson <mail@jasonrobinson.me>
2023-03-04 01:25:34 +0200

    Collectstatic needs running on startup

--- docker/app/daphne.sh
+++ docker/app/daphne.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /usr/local/bin/python /app/manage.py migrate --noinput
+/usr/local/bin/python /app/manage.py collectstatic --noinput
 /usr/local/bin/daphne -p 23564 config.asgi:application -v1 --proxy-headers
