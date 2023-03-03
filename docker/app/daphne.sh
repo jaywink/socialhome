e5dbbfbd6bf3fac958700dc320bb8e3a347df880
Jason Robinson <mail@jasonrobinson.me>
2023-03-04 01:50:34 +0200

    Bind to 0.0.0.0. Don't do hooks in circus_docker.ini

--- docker/app/daphne.sh
+++ docker/app/daphne.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/python /app/manage.py migrate --noinput
 /usr/local/bin/python /app/manage.py collectstatic --noinput
-/usr/local/bin/daphne -p 23564 config.asgi:application -v1 --proxy-headers
+/usr/local/bin/daphne -b 0.0.0.0 -p 23564 config.asgi:application -v1 --proxy-headers
