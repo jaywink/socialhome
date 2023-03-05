5a9556c9d8127e6eac32d7f9d9219f52a020d707
Alain St-Denis <alain@zenfolie.org>
2023-03-05 08:23:44 -0500

    circus.sockets.daphne can't be use to set an env variable from within circus.ini. Pass it as an argument to daphne.sh instead. Shave a few MBs off the docker image.

--- docker/app/daphne.sh
+++ docker/app/daphne.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/python /app/manage.py migrate --noinput
 /usr/local/bin/python /app/manage.py collectstatic --noinput
-/usr/local/bin/daphne --fd $CIRCUS_SOCKET_FD config.asgi:application -v1 --proxy-headers
+/usr/local/bin/daphne --fd $1 config.asgi:application -v1 --proxy-headers
