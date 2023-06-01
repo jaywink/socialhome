c94d213905a0e770354381c7f9497b3d307f036e
Alain St-Denis <alain@zenfolie.org>
2023-06-01 11:02:53 -0400

    Remove useless plugin and console.log calls..

--- socialhome/frontend/src/store/modules/application.js
+++ socialhome/frontend/src/store/modules/application.js
@@ -1,17 +1,6 @@
 /* eslint-disable no-shadow */
 import _get from "lodash/get"
 
-export const applicationPlugin = store => {
-    // called when the store is initialized
-    store.subscribe(({type, payload}) => {
-        // eslint-disable-next-line no-console
-        console.log(type, payload)
-        if (/^stream\/GET[A-Z_]*SUCCEEDED$/.test(type)) {
-            store.commit("profiles/setProfilesFromContentList", payload.payload.data, {root: true})
-        }
-    })
-}
-
 const state = {
     isUserAuthenticated: _get(window, ["context", "isUserAuthenticated"], false),
     currentBrowsingProfileId: _get(window, ["context", "currentBrowsingProfileId"]),
