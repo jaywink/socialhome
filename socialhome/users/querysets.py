07b27c8ba3356e5b9e2b5f15ca70644f25252682
Alain St-Denis <alain@zenfolie.org>
2023-07-27 10:24:14 -0400

    Remove the race condition handling code which is totally broken.

--- socialhome/users/querysets.py
+++ socialhome/users/querysets.py
@@ -41,11 +41,7 @@ class ProfileQuerySet(QuerySet):
             if fid.startswith('http'):
                 values['fid'] = fid
             values.update(extra_lookups)
-            try:
-                return self.create(**values), True
-            except IntegrityError:
-                # another worker won the race?
-                return self.fed_update_or_create(fid, values, extra_lookups, force)
+            return self.create(**values), True
         else:
             changed = False
             for key, value in values.items():
