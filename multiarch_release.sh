07d821e3aa0365fbefb233285d6e2e6c743afabe
Alain St-Denis <alain@zenfolie.org>
2024-02-19 08:25:47 -0500

    Add a build script for multi-architecture container images

---
+++ multiarch_release.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#####################
+# Release to Docker #
+#####################
+#
+# Builds the image and pushes it to the Docker registry.
+#
+# Args:
+#   tag name
+#
+# Pre-requisites:
+# 	- qemu-user-static must be installed
+# 	- bin_fmt kernel support
+# 	- must run:
+# 		sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#	  if it has not been run since the last reboot
+#
+# Note: building foreign platforms might talk a lot more time.
+
+registry=registry.gitlab.com
+platforms=linux/amd64,linux/arm64
+manifest=$registry/jaywink/socialhome
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 tag"
+    exit 1
+else
+    tag=$1
+fi
+
+buildah manifest rm $manifest
+set -e
+
+buildah build -f docker/app/Dockerfile --squash --jobs=2 --platform=$platforms --manifest $manifest .
+
+buildah login $registry
+buildah manifest push --all $manifest docker://$manifest:$tag
+
