ca485bc92d0e5e98ebffdf9f9c319c967ac1fff9
Alain St-Denis <alain@zenfolie.org>
2025-02-21 14:44:00 -0500

    re-add rest.framework token authentication, removes while testing session auth and then forgotten

--- config/settings/common.py
+++ config/settings/common.py
@@ -603,6 +603,7 @@ if log_target == "syslog":
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
