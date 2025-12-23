c4a4c2203431507d127706566cc319aef44e02e8
Alain St-Denis <alain@zenfolie.org>
2025-12-23 08:28:43 -0500

    do not use space as a separator for the pong heartbeat message. no need to use async_to_sync on mark_user_recently_active.

--- socialhome/streams/consumers.py
+++ socialhome/streams/consumers.py
@@ -29,7 +29,7 @@ class StreamConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_add)(self.get_stream_name(), self.channel_name)
         user = self.scope["user"]
         if user and user.is_authenticated:
-            async_to_sync(user.mark_recently_active())
+            user.mark_recently_active()
         super().connect()
 
     def disconnect(self, code):
@@ -45,7 +45,7 @@ class StreamConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
         if data.get("event") == "ping":
-            self.send(text_data=json.dumps({"event": "pong"}))
+            self.send(text_data=json.dumps({"event":"pong"}, separators=(',', ':')))
             user = self.scope["user"]
             if user and user.is_authenticated:
-                async_to_sync(user.mark_recently_active())
+                user.mark_recently_active()
