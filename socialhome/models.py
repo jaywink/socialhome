b7028c2bec28f150f5cc7cd4989925d32214fe58
Jason Robinson <mail@jasonrobinson.me>
2026-04-12 01:59:36 +0300

    More PolicyDocument removals

--- socialhome/models.py
+++ socialhome/models.py
@@ -1,17 +1,11 @@
-import reversion
 from django.db import models
-from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
-from django_fsm import FSMField, transition
-from django_enum import EnumField
-from markdownx.models import MarkdownxField
 from model_utils.models import TimeStampedModel
 
-from socialhome.enums import PolicyDocumentType
 from socialhome.users.models import User
 
 
-# TODO remove
+# Not used in code but required for exports
 class ImageUpload(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="imageuploads")
     image = models.ImageField()
@@ -31,33 +25,3 @@ class MediaUpload(TimeStampedModel):
 
     def __str__(self):
         return self.media.name
-
-
-# TODO remove
-@reversion.register(ignore_duplicates=True)
-class PolicyDocument(TimeStampedModel):
-    content = MarkdownxField()
-    version = models.DateTimeField(auto_now=True)
-    published_content = MarkdownxField(editable=False, blank=True)
-    published_version = models.DateTimeField(editable=False, null=True)
-    state = FSMField(default='draft', choices=[
-        ('draft', 'draft'),
-        ('published', 'published'),
-    ])
-    type = EnumField(PolicyDocumentType, unique=True, max_length=30)
-
-    def __str__(self):
-        return "%s (%s - %s)" % (self.type, self.state, self.version)
-
-    def save(self, *args, **kwargs):
-        with reversion.create_revision():
-            super().save(*args, **kwargs)
-
-    @transition(field=state, source=['draft', 'published'], target='draft')
-    def edit_draft(self):
-        pass
-
-    @transition(field=state, source=['draft', 'published'], target='published')
-    def publish(self):
-        self.published_content = self.content
-        self.published_version = now()
