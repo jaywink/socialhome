0e7dd26e5d6480d50f02e91c9dd9236f90ceac03
Alain St-Denis <alain@zenfolie.org>
2026-04-28 09:04:24 -0400

    make middleware both sync and async contexts aware.

--- socialhome/users/middleware.py
+++ socialhome/users/middleware.py
@@ -1,60 +1,112 @@
+from asgiref.sync import iscoroutinefunction
 import json
 
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import render
+from django.utils.decorators import sync_and_async_middleware
 
 from socialhome.users.models import User
 
-class AdminApprovalMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+@sync_and_async_middleware
+def AdminApprovalMiddleware(get_response):
+    if iscoroutinefunction(get_response):
+        async def middleware(request):
+            response = None
+            admin_approved = None
+            # If we require admin approvals, and the user is not approved, render a waiting page.
+            # Always allow the auth flows, however.
+            if settings.ACCOUNT_SIGNUP_REQUIRE_ADMIN_APPROVAL:
+                if  request.path.startswith('/api/allauth/browser/v1/auth/session') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/login') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/signup') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/email/verify'):
+                    response = await get_response(request)
+                    if response.status_code != 200: return response
+                    try:
+                        content = json.loads(response.content)
+                        user_id = content['data']['user']['id']
+                        user = await User.objects.aget(id=user_id)
+                        admin_approved = getattr(user, 'admin_approved', None)
+                    except KeyError:
+                        user = None
+                if getattr(await request.auser(), "admin_approved", admin_approved) is False and \
+                    not (request.path.startswith('/accounts') \
+                        or request.path.startswith('/admin')):
+                    if request.path.startswith('/api'):
+                        if not response:
+                            request.user = AnonymousUser()
+                            response = await get_response(request)
+                            if response.status_code == 200: return response
+                        response.status_code = 401
+                        response.content  = json.dumps({'status': 401,
+                                                       "data": {
+                                                           "flows":[
+                                                               {
+                                                                   "id": "admin_approval_required",
+                                                                   "is_pending": True
+                                                               }
+                                                           ]
+                                                       },
+                                                       "meta": {
+                                                           "is_authenticated": False
+                                                       }
+                                                       })
+                        response.content_type = "application/json"
+                        return response
+                    else:
+                        return render(request, 'users/admin_approval_required.html')
+                else: return response if response else await get_response(request)
 
-    def __call__(self, request):
-        response = None
-        admin_approved = None
-        # If we require admin approvals, and the user is not approved, render a waiting page.
-        # Always allow the auth flows, however.
-        if settings.ACCOUNT_SIGNUP_REQUIRE_ADMIN_APPROVAL:
-            if  request.path.startswith('/api/allauth/browser/v1/auth/session') \
-                or request.path.startswith('/api/allauth/browser/v1/auth/login') \
-                or request.path.startswith('/api/allauth/browser/v1/auth/signup') \
-                or request.path.startswith('/api/allauth/browser/v1/auth/email/verify'):
-                response = self.get_response(request)
-                if response.status_code != 200: return response
-                try:
-                    content = json.loads(response.content)
-                    user_id = content['data']['user']['id']
-                    user = User.objects.get(id=user_id)
-                    admin_approved = getattr(user, 'admin_approved', None)
-                except KeyError:
-                    user = None
-            if getattr(request.user, "admin_approved", admin_approved) is False and \
-                not (request.path.startswith('/accounts') \
-                    or request.path.startswith('/admin')):
-                if request.path.startswith('/api'):
-                    if not response:
-                        request.user = AnonymousUser()
-                        response = self.get_response(request)
-                        if response.status_code == 200: return response
-                    response.status_code = 401
-                    response.content  = json.dumps({'status': 401,
-                                                   "data": {
-                                                       "flows":[
-                                                           {
-                                                               "id": "admin_approval_required",
-                                                               "is_pending": True
-                                                           }
-                                                       ]
-                                                   },
-                                                   "meta": {
-                                                       "is_authenticated": False
-                                                   }
-                                                   })
-                    response.content_type = "application/json"
-                    return response
-                else:
-                    return render(request, 'users/admin_approval_required.html')
-            else: return response if response else self.get_response(request)
+            else: return await get_response(request)
+    else:
+        def middleware(request):
+            response = None
+            admin_approved = None
+            # If we require admin approvals, and the user is not approved, render a waiting page.
+            # Always allow the auth flows, however.
+            if settings.ACCOUNT_SIGNUP_REQUIRE_ADMIN_APPROVAL:
+                if  request.path.startswith('/api/allauth/browser/v1/auth/session') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/login') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/signup') \
+                    or request.path.startswith('/api/allauth/browser/v1/auth/email/verify'):
+                    response = get_response(request)
+                    if response.status_code != 200: return response
+                    try:
+                        content = json.loads(response.content)
+                        user_id = content['data']['user']['id']
+                        user = User.objects.get(id=user_id)
+                        admin_approved = getattr(user, 'admin_approved', None)
+                    except KeyError:
+                        user = None
+                if getattr(request.user, 'admin_approved', admin_approved) is False and \
+                    not (request.path.startswith('/accounts') \
+                        or request.path.startswith('/admin')):
+                    if request.path.startswith('/api'):
+                        if not response:
+                            request.user = AnonymousUser()
+                            response = get_response(request)
+                            if response.status_code == 200: return response
+                        response.status_code = 401
+                        response.content  = json.dumps({'status': 401,
+                                                       "data": {
+                                                           "flows":[
+                                                               {
+                                                                   "id": "admin_approval_required",
+                                                                   "is_pending": True
+                                                               }
+                                                           ]
+                                                       },
+                                                       "meta": {
+                                                           "is_authenticated": False
+                                                       }
+                                                       })
+                        response.content_type = "application/json"
+                        return response
+                    else:
+                        return render(request, 'users/admin_approval_required.html')
+                else: return response if response else get_response(request)
 
-        else: return self.get_response(request)
+            else: return get_response(request)
+
+    return middleware
