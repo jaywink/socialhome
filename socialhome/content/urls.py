cbc8bafa4fdef88de3f2d970b4f495f921baf677
Alain St-Denis <alain@zenfolie.org>
2026-04-28 08:48:38 -0400

    replace obsolete views with federation ActivitypubObjectView.

--- socialhome/content/urls.py
+++ socialhome/content/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 
+from federation.entities.activitypub.django.views import ActivitypubObjectView
 from socialhome.content import views
 
 app_name = 'content'
@@ -9,7 +10,7 @@ urlpatterns = [
     # /content/123/  # pk
     # /content/123/slug/  # pk + slug
     # /content/abcd-edfg-ffff-aaaa/  # uuid
-    re_path(r"^(?P<pk>[0-9]+)/$", views.ContentView.as_view(), name="view"),
-    re_path(r"^(?P<pk>[0-9]+)/(?P<slug>[-\w]+)/$", views.ContentView.as_view(), name="view-by-slug"),
-    re_path(r"^(?P<uuid>[^/]+)/$", views.ContentView.as_view(), name="view-by-uuid"),
+    re_path(r"^(?P<pk>[0-9]+)/$", ActivitypubObjectView.as_view(), name="view"),
+    re_path(r"^(?P<pk>[0-9]+)/(?P<slug>[-\w]+)/$", ActivitypubObjectView.as_view(), name="view-by-slug"),
+    re_path(r"^(?P<uuid>[^/]+)/$", ActivitypubObjectView.as_view(), name="view-by-uuid"),
 ]
