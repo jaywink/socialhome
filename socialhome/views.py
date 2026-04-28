bc02dfc34d2b629b762e4fa607364d268221591f
Alain St-Denis <alain@zenfolie.org>
2026-04-28 09:02:47 -0400

    remove obsolete views.

--- socialhome/views.py
+++ socialhome/views.py
@@ -1,9 +1,6 @@
 import coreapi
 import coreschema
 
-from django.utils.decorators import method_decorator
-from django.views import View
-from federation.entities.activitypub.django.views import activitypub_object_view
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
