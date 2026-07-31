d5e194dd2da95459a3eafda91ef0c317869e0b1b
Jason Robinson <mail@jasonrobinson.me>
2026-08-01 01:23:04 +0300

    Move apscheduler to a management command
    We can't start it in the Django app initialization, at least not without django-apscheduler. However, running it as it's own process via a management command is anyway probably cleaner.
    
    Added a dummy job every 5 minutes to see some output.

--- socialhome/tasks/apps.py
+++ socialhome/tasks/apps.py
@@ -1,11 +1,6 @@
 import logging
-import sys
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
 from django.apps import AppConfig
-
-from socialhome.streams.tasks import delete_redis_keys, groom_redis_precaches
 
 logger = logging.getLogger("socialhome")
 
@@ -13,28 +8,3 @@ logger = logging.getLogger("socialhome")
 class TasksConfig(AppConfig):
     name = "socialhome.tasks"
     verbose_name = "Tasks"
-
-    def ready(self):
-        # Only register tasks if running dramatiq
-        if "rundramatiq" not in sys.argv:
-            return
-
-        scheduler = BackgroundScheduler()
-
-        # Queue tasks
-        # Clean up some Redis keys
-        logger.info("tasks - Scheduling streams task: delete_redis_keys")
-        scheduler.add_job(
-            func=delete_redis_keys.send,
-            trigger=CronTrigger(hour="*/12", minute=15),
-            args=[["rq:job:*", "rq:results:*", "fed_cache:*"]],
-        )
-        # Groom redis precaches
-        logger.info("tasks - Scheduling streams task: groom_redis_precaches")
-        scheduler.add_job(
-            func=groom_redis_precaches.send,
-            trigger=CronTrigger(hour="*/3", minute=30),
-        )
-
-        # Start the scheduler
-        scheduler.start()
---
+++ socialhome/tasks/management/commands/runscheduler.py
@@ -0,0 +1,59 @@
+import logging
+
+import dramatiq
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+
+from socialhome.streams.tasks import delete_redis_keys, groom_redis_precaches
+
+logger = logging.getLogger("socialhome")
+
+
+@dramatiq.actor
+def dummy_task():
+    logger.info("tasks.dummy_task - Running dummy task")
+
+
+class Command(BaseCommand):
+    help = """Run APScheduler.
+    
+    Note! Only run this once. If using the provided `circus.ini` configuration, or using the
+    provided official Docker image, this scheduler will already be running.
+    """
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler()
+
+        # Queue tasks
+        # Clean up some Redis keys
+        logger.info("tasks - Scheduling streams task: delete_redis_keys")
+        scheduler.add_job(
+            func=delete_redis_keys.send,
+            trigger=CronTrigger.from_crontab("15 */12 * * *"),
+            args=[["rq:job:*", "rq:results:*", "fed_cache:*"]],
+            max_instances=1,
+        )
+        # Groom redis precaches
+        logger.info("tasks - Scheduling streams task: groom_redis_precaches")
+        scheduler.add_job(
+            func=groom_redis_precaches.send,
+            trigger=CronTrigger.from_crontab("30 */3 * * *"),
+            max_instances=1,
+        )
+
+        # Dummy task
+        logger.info("tasks - Scheduling dummy task")
+        scheduler.add_job(
+            func=dummy_task,
+            trigger=CronTrigger.from_crontab("*/5 * * * *"),
+        )
+
+        # Start the scheduler
+        try:
+            logger.info("tasks - Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("tasks - Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("tasks - Scheduler shut down successfully!")
