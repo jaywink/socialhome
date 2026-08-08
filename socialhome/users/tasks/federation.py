0d081f22a7a3df5a3e05fe97cadf2593886d5e3e
Alain St-Denis <alain@zenfolie.org>
2026-08-08 10:48:05 -0400

    make update_profile_from_fed async.

--- socialhome/users/tasks/federation.py
+++ socialhome/users/tasks/federation.py
@@ -6,20 +6,20 @@ logger = logging.getLogger("socialhome")
 
 
 @dramatiq.actor(priority=settings.DRAMATIQ_PRIORITY_LOW)
-def update_profile_from_fed(profile_id, **kwargs):
+async def update_profile_from_fed(profile_id):
     from federation.fetchers import retrieve_remote_profile
     from socialhome.users.models import Profile
 
     try:
-        profile = Profile.objects.get(id=profile_id)
+        profile = await Profile.objects.aget(id=profile_id)
     except Profile.DoesNotExist:
         logger.warning('update_profile - profile id %s not found', profile_id)
         return
 
-    remote_profile = retrieve_remote_profile(profile.fid if profile.fid else profile.handle)
+    remote_profile = await retrieve_remote_profile(profile.fid if profile.fid else profile.handle)
     if remote_profile:
-        Profile.from_remote_profile(remote_profile, force=True)
-        profile.refresh_from_db()
+        await sync_to_async(Profile.from_remote_profile)(remote_profile, force=True)
+        await profile.arefresh_from_db()
         logger.info('update_profile - profile %s updated', profile)
     else:
         logger.warning('update_profile - failed to retrieve %s', profile)
